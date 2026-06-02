113a9d16d14e00103988a488519b2bbf76d61cd7
DIvatulla <ivatulladaniel0@gmail.com>
2026-06-02 16:13:43 +0500

    asdcx

--- main.c
+++ main.c
@@ -2,53 +2,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-static char *device = "plughw:0,0";			/* playback device */
-static snd_pcm_format_t format = SND_PCM_FORMAT_S16;	/* sample format */
-static unsigned int rate = 44100;			/* stream rate */
+static char *device = "plughw:0,1";			/* capture device */
+static snd_pcm_format_t format = SND_PCM_FORMAT_S16_LE;	/* sample format */
+static unsigned int sample_rate = 16000;			/* stream rate */
 static unsigned int channels = 1;			/* count of channels */
-static unsigned int buffer_time = 500000;		/* ring buffer length in us */
-static unsigned int period_time = 100000;		/* period time in us */
-static double freq = 440;				/* sinusoidal wave frequency in Hz */
-static int verbose = 0;					/* verbose flag */
-static int resample = 1;				/* enable alsa-lib resampling */
-static int period_event = 0;				/* produce poll event after each period */
 
-static snd_pcm_sframes_t buffer_size;
-static snd_pcm_sframes_t period_size;
-static snd_output_t *output = NULL;
+static snd_pcm_t *handle; //api connection
+static snd_pcm_hw_params_t *params; //parameters
 
 int main(int argc, char** argv)
-{
-	int val;
+{    
+	int err;
+    
+    int buffer_time = 5;
+    int total_frames = sample_rate * buffer_time;
+    int bytes_per_frame = 2;
+    char *buf = calloc(total_frames * bytes_per_frame, sizeof(char));
 
-	printf("ALSA library version: %s\n", SND_LIB_VERSION_STR);
-
-	printf("\nPCM stream types:\n");
-	for (val = 0; val <= SND_PCM_STREAM_LAST; val++){
-		printf("  %s\n", snd_pcm_stream_name((snd_pcm_stream_t)val));
-	}
-
-	printf("\nPCM access types:\n");
-	for (val = 0; val <= SND_PCM_ACCESS_LAST; val++){
-		printf("  %s\n", snd_pcm_access_name((snd_pcm_access_t)val));
-	}
-
-	printf("\nPCM formats:\n");
-	for (val = 0; val <= SND_PCM_FORMAT_LAST; val++){
-		if (snd_pcm_format_name((snd_pcm_format_t)val) != NULL){
-			printf("  %s (%s)\n", snd_pcm_format_name((snd_pcm_format_t)val),
-				snd_pcm_format_description((snd_pcm_format_t)val));
-		}
-	}
-
-	printf("\nPCM subformats:\n");
-	for (val = 0; val <= SND_PCM_SUBFORMAT_LAST; val++){
-		printf("  %s (%s)\n", snd_pcm_subformat_name((snd_pcm_subformat_t)val),
-	        snd_pcm_subformat_description((snd_pcm_subformat_t)val));
+    err = snd_pcm_open(&handle, device, SND_PCM_STREAM_CAPTURE, 0) //open ALSA connection
+    if (err < 0) {
+        fprintf(stderr, "Can't open device %s\n", device);
+        exit(1);
     }
-	
-	printf("\nPCM states:\n");
-	for (val = 0; val <= SND_PCM_STATE_LAST; val++){
-		printf("  %s\n", snd_pcm_state_name((snd_pcm_state_t)val));
-	}
+
+    snd_pcm_hw_params_malloc(&params); //allocates memory fow device parameters
+    snd_pcm_hw_params_any(handle, params); //get device parameters
 }
